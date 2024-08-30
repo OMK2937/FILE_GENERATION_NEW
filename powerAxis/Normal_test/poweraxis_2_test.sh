f435d7999a8e7d52775b696d93cc31d1a5ecb491
Divij Sharma <divij.sharma@mobikwik.com>
2024-08-30 15:36:21 +0530

    query optimization of poweraxis_2

--- powerAxis/Normal_test/poweraxis_2_test.sh
+++ powerAxis/Normal_test/poweraxis_2_test.sh
@@ -5,134 +5,86 @@ Queries()
 
 #AXIS NODAL MP WORKING FILE
 echo "
-select wapg.mid,
-        wapg.smid,
-        wapg.orderid,
-        wapg.createdat as txntime,
-        wapg.txnamount as amount,
-        wapg.totalpayoutamt,
-        -(- wapg.totalpayoutamt) as settlement_amount,
-        date_format(now(), '%Y-%m-%d') as settlement_date,
-        payoutbatchid as settlement_batch,
-        'credit' as type,
-        wapg.wallettxnamount as walletAmount,
-        wapg.pgtxnamount as PgTxnAmount,
-caSe
-                when ppo.gatewayid = '15' then 'UPI'
-                when ppo.gatewayid = '16' then 'ZIP' else 'WAPG'
-        end aS 'TxnMode'
-from wallet_as_pg_ledger wapg force index(idx_wallet_as_pg_ledger_updatedat)
-        left join wallet_as_pg_ledger_metadata wapgm force index(PRIMARY) on (wapg.id = wapgm.parentid)
-        left join pg_payment_order ppo force index(idx_ppo_stamp) on (wapgm.pgorderid = ppo.stamp)
-        left join merchant on (wapg.mid = merchant.mid)
-where (
-                wapg.mid not in (
-                        select mid
-                        from icici_payout_merchants
-                        where isPayoutEnabled = 1
-                )
-                and if(
-                        wapg.smid is not NULL,
-                        wapg.smid not in (
-                                select mid
-                                from icici_payout_merchants
-                                where isPayoutEnabled = 1
-                        ),
-(
-                                wapg.mid not in (
-                                        select mid
-                                        from icici_payout_merchants
-                                        where isPayoutEnabled = 1
-                                )
-                        )
-                )
-        )
-        and wapg.updatedat >= DATE(now())
-        and payoutbatchid like concat('%', date_format(now(), '%Y%m%d'), '%')
-        and ismarketplace = 'y'
-        and paymentType not in (2, 3,4)
-        and payoutbatchid in (
-                select distinct(batch_id)
-                from settlement_wapg
-                where created_at >= curdate() and status = 'calculated'
-        )
-        and (
-                wapg.mid in (
-                        select mid
-                        from merchant_payout_config
-                        where power_access_file = 1
-                )
-                or wapg.smid in (
-                        select mid
-                        from merchant_payout_config
-                        where power_access_file = 1
-                )
-        )
-union all
-select wapg.mid,
-        wapg.smid,
-        wapg.orderid,
-        wapg.createdat as txntime,
-        wapg.txnamount as amount,
-        wapg.txnamount,
-        - wapg.txnamount as settlement_amount,
-        date_format(now(), '%Y-%m-%d') as settlement_date,
-        wapg.refundbatchid as settlement_batch,
-        'debit' as type,
-        wapg.wallettxnamount as walletAmount,
-        wapg.pgtxnamount as PgTxnAmount,
-caSe
-                when ppo.gatewayid = '15' then 'UPI'
-                when ppo.gatewayid = '16' then 'ZIP' else 'WAPG'
-        end aS 'TxnMode'
-from wallet_as_pg_ledger wapg force index(idx_wallet_as_pg_ledger_updatedat)
-        left join wallet_as_pg_ledger_metadata wapgm force index(PRIMARY) on (wapg.id = wapgm.parentid)
-        left join pg_payment_order ppo force index(idx_ppo_stamp) on (wapgm.pgorderid = ppo.stamp)
-        left join merchant on (wapg.mid = merchant.mid)
-where (
-                wapg.mid not in (
-                        select mid
-                        from icici_payout_merchants
-                        where isPayoutEnabled = 1
-                )
-                and if(
-                        wapg.smid is not NULL,
-                        wapg.smid not in (
-                                select mid
-                                from icici_payout_merchants
-                                where isPayoutEnabled = 1
-                        ),
-(
-                                wapg.mid not in (
-                                        select mid
-                                        from icici_payout_merchants
-                                        where isPayoutEnabled = 1
-                                )
-                        )
-                )
-        )
-        and wapg.updatedat >= DATE(now())
-        and refundbatchid like concat('%', date_format(now(), '%Y%m%d'), '%')
-        and ismarketplace = 'y'
-        and paymentType not in (2, 3,4)
-        and refundbatchid in (
-                select distinct(batch_id)
-                from settlement_wapg
-                where created_at >= curdate() and  status = 'calculated'
-        )
-        and (
-                wapg.mid in (
-                        select mid
-                        from merchant_payout_config
-                        where power_access_file = 1
-                )
-                or wapg.smid in (
-                        select mid
-                        from merchant_payout_config
-                        where power_access_file = 1
-                )
-        )
-order by 1;
+SELECT wapg.mid,
+       wapg.smid,
+       wapg.orderid,
+       wapg.createdat                 AS txntime,
+       wapg.txnamount                 AS amount,
+       wapg.totalpayoutamt,
+       -( -wapg.totalpayoutamt )      AS settlement_amount,
+       Date_format(Now(), '%Y-%m-%d') AS settlement_date,
+       payoutbatchid                  AS settlement_batch,
+       'credit'                       AS type,
+       wapg.wallettxnamount           AS walletAmount,
+       wapg.pgtxnamount               AS PgTxnAmount,
+       CASE
+         WHEN ppo.gatewayid = '15' THEN 'UPI'
+         WHEN ppo.gatewayid = '16' THEN 'ZIP'
+         ELSE 'WAPG'
+       end                            AS 'TxnMode'
+FROM   wallet_as_pg_ledger wapg FORCE INDEX(idx_wallet_as_pg_ledger_updatedat)
+       LEFT JOIN wallet_as_pg_ledger_metadata wapgm ON ( wapg.id = wapgm.parentid )
+       LEFT JOIN pg_payment_order ppo ON ( wapgm.pgorderid = ppo.stamp )
+       LEFT JOIN merchant ON ( wapg.mid = merchant.mid )
+       LEFT JOIN icici_payout_merchants ipm1 ON wapg.mid = ipm1.mid AND ipm1.ispayoutenabled = 1
+       LEFT JOIN icici_payout_merchants ipm2 ON wapg.smid = ipm2.mid AND ipm2.ispayoutenabled = 1
+WHERE  ipm1.mid IS NULL
+       AND ipm2.mid IS NULL
+       AND wapg.updatedat >= Date(Now())
+       AND payoutbatchid LIKE Concat('%', Date_format(Now(), '%Y%m%d'), '%')
+       AND ismarketplace = 'y'
+       AND paymenttype NOT IN ( 2, 3, 4 )
+       AND payoutbatchid IN (SELECT DISTINCT(batch_id)
+                             FROM   settlement_wapg
+                             WHERE  created_at >= Curdate()
+                                    AND status = 'success')
+       AND ( wapg.mid IN (SELECT mid
+                          FROM   merchant_payout_config
+                          WHERE  power_access_file = 1)
+              OR wapg.smid IN (SELECT mid
+                               FROM   merchant_payout_config
+                               WHERE  power_access_file = 1) )
+UNION ALL
+SELECT wapg.mid,
+       wapg.smid,
+       wapg.orderid,
+       wapg.createdat                 AS txntime,
+       wapg.txnamount                 AS amount,
+       wapg.txnamount,
+       -wapg.txnamount                AS settlement_amount,
+       Date_format(Now(), '%Y-%m-%d') AS settlement_date,
+       wapg.refundbatchid             AS settlement_batch,
+       'debit'                        AS type,
+       wapg.wallettxnamount           AS walletAmount,
+       wapg.pgtxnamount               AS PgTxnAmount,
+       CASE
+         WHEN ppo.gatewayid = '15' THEN 'UPI'
+         WHEN ppo.gatewayid = '16' THEN 'ZIP'
+         ELSE 'WAPG'
+       end                            AS 'TxnMode'
+FROM   wallet_as_pg_ledger wapg FORCE INDEX(idx_wallet_as_pg_ledger_updatedat)
+       LEFT JOIN wallet_as_pg_ledger_metadata wapgm ON ( wapg.id = wapgm.parentid )
+       LEFT JOIN pg_payment_order ppo ON ( wapgm.pgorderid = ppo.stamp )
+       LEFT JOIN merchant ON ( wapg.mid = merchant.mid )
+       LEFT JOIN icici_payout_merchants ipm1 ON wapg.mid = ipm1.mid AND ipm1.ispayoutenabled = 1
+       LEFT JOIN icici_payout_merchants ipm2 ON wapg.smid = ipm2.mid AND ipm2.ispayoutenabled = 1
+WHERE  ipm1.mid IS NULL
+       AND ipm2.mid IS NULL
+       AND wapg.updatedat >= Date(Now())
+       AND refundbatchid LIKE Concat('%', Date_format(Now(), '%Y%m%d'), '%')
+       AND ismarketplace = 'y'
+       AND paymenttype NOT IN ( 2, 3, 4 )
+       AND refundbatchid IN (SELECT DISTINCT(batch_id)
+                             FROM   settlement_wapg
+                             WHERE  created_at >= Curdate()
+                                    AND status = 'success')
+       AND ( wapg.mid IN (SELECT mid
+                          FROM   merchant_payout_config
+                          WHERE  power_access_file = 1)
+              OR wapg.smid IN (SELECT mid
+                               FROM   merchant_payout_config
+                               WHERE  power_access_file = 1) )
+ORDER  BY 1;
 "| $MYSQL --login-path=mobinewcronmaster_RDS01 -D $DB | sed 's/\t/","/g;s/^/"/;s/$/"/;s/\n//g' > /data/cronreport-payout/AXIS_NODAL_MP_WORKING_FILE_test.csv
 
 
