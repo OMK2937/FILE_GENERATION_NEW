6c3857722aacb85d1907cb8fbb9038185c2b8b73
Divij Sharma <divij.sharma@mobikwik.com>
2024-09-12 18:19:55 +0530

    icici automated and normal files changes

--- ICICI/Automated/icici_nodal_payout_file_generation_marketplace_automated.sh
+++ ICICI/Automated/icici_nodal_payout_file_generation_marketplace_automated.sh
@@ -46,16 +46,178 @@ End
 #path=${COMPRESSDIR}/Payment_Benificiary_ICICI_MBK_$CURRENTDATE.csv
 #curl -H "userId:nocserver@mobikwik.com" -H "Content-Type:multipart/form-data" -F "file=@$path" https://beta2.mobikwik.com/walletapis/merchantpanel/payouts/upload
 #ICICI
-echo "select wapg.mid,wapg.smid,wapg.orderid,wapg.createdat as txntime,wapg.txnamount as amount,wapg.totalpayoutamt, -(-wapg.totalpayoutamt) as settlement_amount,date_format(now(),'%Y-%m-%d') as settlement_date,wapg.payoutbatchid as settlement_batch,'credit' as type, wapg.wallettxnamount as walletAmount,wapg.pgtxnamount as PgTxnAmount,caSe when wapgm.paymentType ='1' then 'UPI' when wapgm.paymentType in ('2','3') then 'ZIP' else 'WAPG' end aS 'TxnMode', case when collection_mode = '3' then 'Bijlipay_EDC' else 'Mobikwik' end AS Collection_Mode,
-(case when payment_instrument='0' then 'Wallet' when payment_instrument='1' then 'WALLET_AND_PG' when payment_instrument='2' then 'PG' when payment_instrument='3' then 'PAYLATER' when payment_instrument='4' then 'ZIP_AND_WALLET' when payment_instrument='5' then 'ZIP_EMI' when payment_instrument='6' then 'UPI' when payment_instrument='7' then 'CC' when payment_instrument='8' then 'DC' when payment_instrument='9' then 'CC_DC' when payment_instrument='10' then 'UPI_COLLECT' when payment_instrument='11' then 'EMANDATE' when payment_instrument='12' then 'NET_BANKING' else null end) AS Payment_Instrument,
-mtmd.ext_ref_no AS 'External_Refrence_Number'
-   from wallet_as_pg_ledger wapg left join wallet_as_pg_ledger_metadata wapgm on (wapg.id=wapgm.parentid) left join txp t on (t.orderid=wapg.orderid and t.mid=wapg.mid and t.statecode between 28 and 68)
-left join merchant_txp_meta_data mtmd on (t.id=mtmd.parent_id) left join merchant mm on(wapg.mid=mm.mid) where (wapg.mid in (select mid from icici_payout_merchants  where isPayoutEnabled=1) or if(wapg.smid is not NULL, wapg.smid in (select mid from icici_payout_merchants  where isPayoutEnabled=1),(wapg.mid in (select mid from icici_payout_merchants  where isPayoutEnabled=1)))) and wapg.payoutbatchid like concat('%',date_format(now(),'%Y%m%d'),'%') and wapg.payoutbatchid IN (SELECT DISTINCT( batch_id ) FROM  settlement_wapg WHERE  (status IN ('automated_success', 'automated_failure', 'automated_pending', 'automated_confirm_failure'))  and created_at >= Curdate()) and ismarketplace ='y' and paymentType not in (2,3,4) and wapg.updatedat>=date(now()) and wapg.settlementdate>=date(now())
-union all
-select wapg.mid,wapg.smid,wapg.orderid,wapg.createdat as txntime,wapg.txnamount as amount,wapg.txnamount, -wapg.txnamount as settlement_amount,date_format(now(),'%Y-%m-%d') as settlement_date, wapg.refundbatchid as settlement_batch,'debit' as type, wapg.wallettxnamount as walletAmount,wapg.pgtxnamount as PgTxnAmount,caSe when wapgm.paymentType ='1' then 'UPI' when wapgm.paymentType in ('2','3') then 'ZIP' else 'WAPG' end aS 'TxnMode',case when collection_mode = '3' then 'Bijlipay_EDC' else 'Mobikwik' end AS Collection_Mode,
-(case when payment_instrument='0' then 'Wallet' when payment_instrument='1' then 'WALLET_AND_PG' when payment_instrument='2' then 'PG' when payment_instrument='3' then 'PAYLATER' when payment_instrument='4' then 'ZIP_AND_WALLET' when payment_instrument='5' then 'ZIP_EMI' when payment_instrument='6' then 'UPI' when payment_instrument='7' then 'CC' when payment_instrument='8' then 'DC' when payment_instrument='9' then 'CC_DC' when payment_instrument='10' then 'UPI_COLLECT' when payment_instrument='11' then 'EMANDATE' when payment_instrument='12' then 'NET_BANKING' else null end) AS Payment_Instrument,
-mtmd.ext_ref_no AS 'External_Refrence_Number'  from wallet_as_pg_ledger wapg left join wallet_as_pg_ledger_metadata wapgm on (wapg.id=wapgm.parentid) left join txp t on (t.orderid=wapg.orderid and t.mid=wapg.mid and t.statecode between 28 and 68)
-left join merchant_txp_meta_data mtmd on (t.id=mtmd.parent_id)  left join merchant mm on(wapg.mid=mm.mid) where (wapg.mid in (select mid from icici_payout_merchants  where isPayoutEnabled=1) or if(wapg.smid is not NULL, wapg.smid in (select mid from icici_payout_merchants  where isPayoutEnabled=1),(wapg.mid in (select mid from icici_payout_merchants  where isPayoutEnabled=1)))) and wapg.refundbatchid like concat('%',date_format(now(),'%Y%m%d'),'%') and wapg.refundbatchid IN (SELECT DISTINCT( batch_id ) FROM  settlement_wapg WHERE (status IN ('automated_success', 'automated_failure', 'automated_pending', 'automated_confirm_failure'))  and created_at >= Curdate()) and  ismarketplace='y' and paymentType not in (2,3,4) and wapg.updatedat>=date(now()) and wapg.settlementdate>=date(now())  order by 1"  | $MYSQL --login-path=mobinewcronmaster_RDS01 -D $DB | sed 's/\t/","/g;s/^/"/;s/$/"/;s/\n//g' >> /tmp/cronreports/working/WAPG_ICICI_Working_file_Mplace_automated_$CURRENTDATE.csv
+echo "SELECT wapg.mid,
+       wapg.smid,
+       wapg.orderid,
+       wapg.createdat                 AS txntime,
+       wapg.txnamount                 AS amount,
+       wapg.totalpayoutamt,
+       -( -wapg.totalpayoutamt )      AS settlement_amount,
+       Date_format(Now(), '%Y-%m-%d') AS settlement_date,
+       wapg.payoutbatchid             AS settlement_batch,
+       'credit'                       AS type,
+       wapg.wallettxnamount           AS walletAmount,
+       wapg.pgtxnamount               AS PgTxnAmount,
+       CASE
+         WHEN wapgm.paymenttype = '1' THEN 'UPI'
+         WHEN wapgm.paymenttype IN ( '2', '3' ) THEN 'ZIP'
+         ELSE 'WAPG'
+       end                            AS 'TxnMode',
+       CASE
+         WHEN collection_mode = '3' THEN 'Bijlipay_EDC'
+         ELSE 'Mobikwik'
+       end                            AS Collection_Mode,
+       ( CASE
+           WHEN payment_instrument = '0' THEN 'Wallet'
+           WHEN payment_instrument = '1' THEN 'WALLET_AND_PG'
+           WHEN payment_instrument = '2' THEN 'PG'
+           WHEN payment_instrument = '3' THEN 'PAYLATER'
+           WHEN payment_instrument = '4' THEN 'ZIP_AND_WALLET'
+           WHEN payment_instrument = '5' THEN 'ZIP_EMI'
+           WHEN payment_instrument = '6' THEN 'UPI'
+           WHEN payment_instrument = '7' THEN 'CC'
+           WHEN payment_instrument = '8' THEN 'DC'
+           WHEN payment_instrument = '9' THEN 'CC_DC'
+           WHEN payment_instrument = '10' THEN 'UPI_COLLECT'
+           WHEN payment_instrument = '11' THEN 'EMANDATE'
+           WHEN payment_instrument = '12' THEN 'NET_BANKING'
+           ELSE NULL
+         end )                        AS Payment_Instrument,
+       mtmd.ext_ref_no                AS 'External_Refrence_Number'
+FROM   wallet_as_pg_ledger wapg
+       LEFT JOIN wallet_as_pg_ledger_metadata wapgm
+              ON ( wapg.id = wapgm.parentid )
+       LEFT JOIN txp t
+              ON ( t.orderid = wapg.orderid
+                   AND t.mid = wapg.mid
+                   AND t.statecode BETWEEN 28 AND 68 )
+       LEFT JOIN merchant_txp_meta_data mtmd
+              ON ( t.id = mtmd.parent_id )
+       LEFT JOIN merchant mm
+              ON( wapg.mid = mm.mid )
+WHERE  ( wapg.mid IN (SELECT mid
+                      FROM   icici_payout_merchants
+                      WHERE  ispayoutenabled = 1)
+          OR IF(wapg.smid IS NOT NULL,
+             wapg.smid IN (SELECT mid
+                           FROM   icici_payout_merchants
+                           WHERE  ispayoutenabled = 1), (
+                 wapg.mid IN (SELECT mid
+                              FROM   icici_payout_merchants
+                              WHERE  ispayoutenabled = 1) )) )
+       AND wapg.payoutbatchid LIKE Concat('%', Date_format(Now(), '%Y%m%d'), '%')
+        AND (
+        (wapg.payoutbatchid, wapg.mid) in (
+        select
+        batch_id, merchant_id
+        from
+        settlement_wapg
+        where
+        created_at >= curdate()
+        and status IN ('automated_success', 'automated_failure', 'automated_pending', 'automated_confirm_failure')
+        )
+        OR
+        (wapg.payoutbatchid, wapg.smid) in (
+        select
+        batch_id, merchant_id
+        from
+        settlement_wapg
+        where
+        created_at >= curdate()
+        and status IN ('automated_success', 'automated_failure', 'automated_pending', 'automated_confirm_failure')
+        )
+        )
+       AND ismarketplace = 'y'
+       AND paymenttype NOT IN ( 2, 3, 4 )
+       AND wapg.updatedat >= Date(Now())
+       AND wapg.settlementdate >= Date(Now())
+UNION
+SELECT wapg.mid,
+       wapg.smid,
+       wapg.orderid,
+       wapg.createdat                 AS txntime,
+       wapg.txnamount                 AS amount,
+       wapg.txnamount,
+       -wapg.txnamount                AS settlement_amount,
+       Date_format(Now(), '%Y-%m-%d') AS settlement_date,
+       wapg.refundbatchid             AS settlement_batch,
+       'debit'                        AS type,
+       wapg.wallettxnamount           AS walletAmount,
+       wapg.pgtxnamount               AS PgTxnAmount,
+       CASE
+         WHEN wapgm.paymenttype = '1' THEN 'UPI'
+         WHEN wapgm.paymenttype IN ( '2', '3' ) THEN 'ZIP'
+         ELSE 'WAPG'
+       end                            AS 'TxnMode',
+       CASE
+         WHEN collection_mode = '3' THEN 'Bijlipay_EDC'
+         ELSE 'Mobikwik'
+       end                            AS Collection_Mode,
+       ( CASE
+           WHEN payment_instrument = '0' THEN 'Wallet'
+           WHEN payment_instrument = '1' THEN 'WALLET_AND_PG'
+           WHEN payment_instrument = '2' THEN 'PG'
+           WHEN payment_instrument = '3' THEN 'PAYLATER'
+           WHEN payment_instrument = '4' THEN 'ZIP_AND_WALLET'
+           WHEN payment_instrument = '5' THEN 'ZIP_EMI'
+           WHEN payment_instrument = '6' THEN 'UPI'
+           WHEN payment_instrument = '7' THEN 'CC'
+           WHEN payment_instrument = '8' THEN 'DC'
+           WHEN payment_instrument = '9' THEN 'CC_DC'
+           WHEN payment_instrument = '10' THEN 'UPI_COLLECT'
+           WHEN payment_instrument = '11' THEN 'EMANDATE'
+           WHEN payment_instrument = '12' THEN 'NET_BANKING'
+           ELSE NULL
+         end )                        AS Payment_Instrument,
+       mtmd.ext_ref_no                AS 'External_Refrence_Number'
+FROM   wallet_as_pg_ledger wapg
+       LEFT JOIN wallet_as_pg_ledger_metadata wapgm
+              ON ( wapg.id = wapgm.parentid )
+       LEFT JOIN txp t
+              ON ( t.orderid = wapg.orderid
+                   AND t.mid = wapg.mid
+                   AND t.statecode BETWEEN 28 AND 68 )
+       LEFT JOIN merchant_txp_meta_data mtmd
+              ON ( t.id = mtmd.parent_id )
+       LEFT JOIN merchant mm
+              ON( wapg.mid = mm.mid )
+WHERE  ( wapg.mid IN (SELECT mid
+                      FROM   icici_payout_merchants
+                      WHERE  ispayoutenabled = 1)
+          OR IF(wapg.smid IS NOT NULL,
+             wapg.smid IN (SELECT mid
+                           FROM   icici_payout_merchants
+                           WHERE  ispayoutenabled = 1), (
+                 wapg.mid IN (SELECT mid
+                              FROM   icici_payout_merchants
+                              WHERE  ispayoutenabled = 1) )) )
+        AND wapg.refundbatchid LIKE Concat('%', Date_format(Now(), '%Y%m%d'), '%')
+        AND (
+        (wapg.refundbatchid, wapg.mid) in (
+        select
+        batch_id, merchant_id
+        from
+        settlement_wapg
+        where
+        created_at >= curdate()
+        and status IN ('automated_success', 'automated_failure', 'automated_pending', 'automated_confirm_failure')
+        )
+        OR
+        (wapg.refundbatchid, wapg.smid) in (
+        select
+        batch_id, merchant_id
+        from
+        settlement_wapg
+        where
+        created_at >= curdate()
+        and status IN ('automated_success', 'automated_failure', 'automated_pending', 'automated_confirm_failure')
+        )
+        )
+       AND ismarketplace = 'y'
+       AND paymenttype NOT IN ( 2, 3, 4 )
+       AND wapg.updatedat >= Date(Now())
+       AND wapg.settlementdate >= Date(Now())
+ORDER  BY 1;"  | $MYSQL --login-path=mobinewcronmaster_RDS01 -D $DB | sed 's/\t/","/g;s/^/"/;s/$/"/;s/\n//g' >> /tmp/cronreports/working/WAPG_ICICI_Working_file_Mplace_automated_$CURRENTDATE.csv
 TIMESTAMP=`date "+%Y-%m-%d %H-%M-%S"`
 echo "Count of merchants : $count and Payout Amount :$amount" |  mail -s "Payout Generated for WAPG Payout | Marketplace Merchants | ICICI Bank | $TIMESTAMP" -r noc@mobikwik.com walletops@mobikwik.com merc-common@mobikwik.com merc@mobikwik.com shashank.v@mobikwik.com mpr@mobikwik.com sre@mobikwik.com
 gzip  /tmp/cronreports/working/WAPG_ICICI_Working_file_Mplace_automated_$CURRENTDATE.csv
--- ICICI/Normal/icici_nodal_payout_file_generation_non_marketplace.sh
+++ ICICI/Normal/icici_nodal_payout_file_generation_non_marketplace.sh
@@ -49,21 +49,180 @@ bye
 End
 
 #ICICI
-echo "select wapg.mid,wapg.smid,wapg.orderid,wapg.createdat as txntime,wapg.txnamount as amount,wapg.totalpayoutamt, -(-wapg.totalpayoutamt) as settlement_amount,date_format(now(),'%Y-%m-%d') as settlement_date,wapg.payoutbatchid as settlement_batch,'credit' as type, wapg.wallettxnamount as walletAmount,wapg.pgtxnamount as PgTxnAmount,case when wapgm.paymentType ='1' then 'UPI' when wapgm.paymentType in ('2','3') then 'ZIP' else 'WAPG' end as 'TxnMode', case when collection_mode = '3' then 'Bijlipay_EDC' else 'Mobikwik' end as Collection_Mode,
-(case when payment_instrument='0' then 'Wallet' when payment_instrument='1' then 'WALLET_AND_PG' when payment_instrument='2' then 'PG' when payment_instrument='3' then 'PAYLATER' when payment_instrument='4' then 'ZIP_AND_WALLET' when payment_instrument='5' then 'ZIP_EMI' when payment_instrument='6' then 'UPI' when payment_instrument='7' then 'CC' when payment_instrument='8' then 'DC' when payment_instrument='9' then 'CC_DC' when payment_instrument='10' then 'UPI_COLLECT' when payment_instrument='11' then 'EMANDATE' when payment_instrument='12' then 'NET_BANKING' else null end) as Payment_Instrument,
-mtmd.ext_ref_no as 'External_Refrence_Number'
-   from wallet_as_pg_ledger wapg
-   left join txp t on (t.orderid=wapg.orderid and t.statecode between 28 and 68)
-   left join merchant_txp_meta_data mtmd on (t.id=mtmd.parent_id and t.statecode between 28 and 68)
-   left join wallet_as_pg_ledger_metadata wapgm on (wapg.id=wapgm.parentid)
-   left join merchant on (wapg.mid=merchant.mid ) where (wapg.mid  in (select mid from icici_payout_merchants  where isPayoutEnabled=1) and if(wapg.smid is not  NULL, wapg.smid  in (select mid from icici_payout_merchants  where isPayoutEnabled=1),(wapg.mid in (select mid from icici_payout_merchants  where isPayoutEnabled=1)))) and wapg.updatedat >= DATE(now()) and wapg.payoutbatchid like concat('%',date_format(now(),'%Y%m%d'),'%') and (ismarketplace !='y' or ismarketplace is null) and paymentType not in (2,3,4) and  wapg.payoutbatchid in (select distinct(batch_id) from settlement_wapg where created_at >= curdate() and status ='calculated')
-union all
-select wapg.mid,wapg.smid,wapg.orderid,wapg.createdat as txntime,wapg.txnamount as amount,wapg.txnamount, -wapg.txnamount as settlement_amount,date_format(now(),'%Y-%m-%d') as settlement_date, wapg.refundbatchid as settlement_batch,'debit' as type, wapg.wallettxnamount as walletAmount,wapg.pgtxnamount as PgTxnAmount,case when wapgm.paymentType ='1' then 'UPI' when wapgm.paymentType in ('2','3') then 'ZIP' else 'WAPG' end as 'TxnMode' , cASe when collection_mode = '3' then 'Bijlipay_EDC' else 'Mobikwik' end AS Collection_Mode,
-(cASe when payment_instrument='0' then 'Wallet' when payment_instrument='1' then 'WALLET_AND_PG' when payment_instrument='2' then 'PG' when payment_instrument='3' then 'PAYLATER' when payment_instrument='4' then 'ZIP_AND_WALLET' when payment_instrument='5' then 'ZIP_EMI' when payment_instrument='6' then 'UPI' when payment_instrument='7' then 'CC' when payment_instrument='8' then 'DC' when payment_instrument='9' then 'CC_DC' when payment_instrument='10' then 'UPI_COLLECT' when payment_instrument='11' then 'EMANDATE' when payment_instrument='12' then 'NET_BANKING' else null end) AS Payment_Instrument,
-mtmd.ext_ref_no AS 'External_Refrence_Number'  from wallet_as_pg_ledger wapg left join wallet_as_pg_ledger_metadata wapgm on (wapg.id=wapgm.parentid)
- left join txp t on (t.orderid=wapg.orderid and t.statecode between 28 and 68)
- left join merchant_txp_meta_data mtmd on (t.id=mtmd.parent_id)
- left join merchant on (wapg.mid=merchant.mid )  where (wapg.mid  in (select mid from icici_payout_merchants  where isPayoutEnabled=1) and if(wapg.smid is not NULL, wapg.smid  in (select mid from icici_payout_merchants  where isPayoutEnabled=1),(wapg.mid  in (select mid from icici_payout_merchants  where isPayoutEnabled=1)))) and  wapg.updatedat >= DATE(now()) and wapg.refundbatchid like concat('%',date_format(now(),'%Y%m%d'),'%') and (ismarketplace !='y' or ismarketplace is null) and paymentType not in (2,3,4) and wapg.refundbatchid in (select distinct(batch_id) from settlement_wapg where created_at >= curdate() and status ='calculated') order by 1"  | $MYSQL --login-path=mobinewcronmaster_RDS01 -D $DB | sed 's/\t/","/g;s/^/"/;s/$/"/;s/\n//g' >> /tmp/cronreports/working/WAPG_ICICI_Working_file_Non_Mplace_$CURRENTDATE.csv
+echo "SELECT wapg.mid,
+       wapg.smid,
+       wapg.orderid,
+       wapg.createdat                 AS txntime,
+       wapg.txnamount                 AS amount,
+       wapg.totalpayoutamt,
+       -( -wapg.totalpayoutamt )      AS settlement_amount,
+       Date_format(Now(), '%Y-%m-%d') AS settlement_date,
+       wapg.payoutbatchid             AS settlement_batch,
+       'credit'                       AS type,
+       wapg.wallettxnamount           AS walletAmount,
+       wapg.pgtxnamount               AS PgTxnAmount,
+       CASE
+         WHEN wapgm.paymenttype = '1' THEN 'UPI'
+         WHEN wapgm.paymenttype IN ( '2', '3' ) THEN 'ZIP'
+         ELSE 'WAPG'
+       end                            AS 'TxnMode',
+       CASE
+         WHEN collection_mode = '3' THEN 'Bijlipay_EDC'
+         ELSE 'Mobikwik'
+       end                            AS Collection_Mode,
+       ( CASE
+           WHEN payment_instrument = '0' THEN 'Wallet'
+           WHEN payment_instrument = '1' THEN 'WALLET_AND_PG'
+           WHEN payment_instrument = '2' THEN 'PG'
+           WHEN payment_instrument = '3' THEN 'PAYLATER'
+           WHEN payment_instrument = '4' THEN 'ZIP_AND_WALLET'
+           WHEN payment_instrument = '5' THEN 'ZIP_EMI'
+           WHEN payment_instrument = '6' THEN 'UPI'
+           WHEN payment_instrument = '7' THEN 'CC'
+           WHEN payment_instrument = '8' THEN 'DC'
+           WHEN payment_instrument = '9' THEN 'CC_DC'
+           WHEN payment_instrument = '10' THEN 'UPI_COLLECT'
+           WHEN payment_instrument = '11' THEN 'EMANDATE'
+           WHEN payment_instrument = '12' THEN 'NET_BANKING'
+           ELSE NULL
+         end )                        AS Payment_Instrument,
+       mtmd.ext_ref_no                AS 'External_Refrence_Number'
+FROM   wallet_as_pg_ledger wapg
+       LEFT JOIN txp t
+              ON ( t.orderid = wapg.orderid
+                   AND t.statecode BETWEEN 28 AND 68 )
+       LEFT JOIN merchant_txp_meta_data mtmd
+              ON ( t.id = mtmd.parent_id
+                   AND t.statecode BETWEEN 28 AND 68 )
+       LEFT JOIN wallet_as_pg_ledger_metadata wapgm
+              ON ( wapg.id = wapgm.parentid )
+       LEFT JOIN merchant
+              ON ( wapg.mid = merchant.mid )
+WHERE  ( wapg.mid IN (SELECT mid
+                      FROM   icici_payout_merchants
+                      WHERE  ispayoutenabled = 1)
+         AND IF(wapg.smid IS NOT NULL,
+             wapg.smid IN (SELECT mid
+                           FROM   icici_payout_merchants
+                           WHERE  ispayoutenabled = 1), (
+                 wapg.mid IN (SELECT mid
+                              FROM   icici_payout_merchants
+                              WHERE  ispayoutenabled = 1) )) )
+       AND wapg.updatedat >= Date(Now())
+       AND wapg.payoutbatchid LIKE Concat('%', Date_format(Now(), '%Y%m%d'), '%'
+                                   )
+       AND ( ismarketplace != 'y'
+              OR ismarketplace IS NULL )
+       AND paymenttype NOT IN ( 2, 3, 4 )
+        AND (
+        (wapg.payoutbatchid, wapg.mid) in (
+        select
+        batch_id, merchant_id
+        from
+        settlement_wapg
+        where
+        created_at >= curdate()
+        and status = 'calculated'
+        )
+        OR
+        (wapg.payoutbatchid, wapg.smid) in (
+        select
+        batch_id, merchant_id
+        from
+        settlement_wapg
+        where
+        created_at >= curdate()
+        and status = 'calculated'
+        )
+        )
+UNION
+SELECT wapg.mid,
+       wapg.smid,
+       wapg.orderid,
+       wapg.createdat                 AS txntime,
+       wapg.txnamount                 AS amount,
+       wapg.txnamount,
+       -wapg.txnamount                AS settlement_amount,
+       Date_format(Now(), '%Y-%m-%d') AS settlement_date,
+       wapg.refundbatchid             AS settlement_batch,
+       'debit'                        AS type,
+       wapg.wallettxnamount           AS walletAmount,
+       wapg.pgtxnamount               AS PgTxnAmount,
+       CASE
+         WHEN wapgm.paymenttype = '1' THEN 'UPI'
+         WHEN wapgm.paymenttype IN ( '2', '3' ) THEN 'ZIP'
+         ELSE 'WAPG'
+       end                            AS 'TxnMode',
+       CASE
+         WHEN collection_mode = '3' THEN 'Bijlipay_EDC'
+         ELSE 'Mobikwik'
+       end                            AS Collection_Mode,
+       ( CASE
+           WHEN payment_instrument = '0' THEN 'Wallet'
+           WHEN payment_instrument = '1' THEN 'WALLET_AND_PG'
+           WHEN payment_instrument = '2' THEN 'PG'
+           WHEN payment_instrument = '3' THEN 'PAYLATER'
+           WHEN payment_instrument = '4' THEN 'ZIP_AND_WALLET'
+           WHEN payment_instrument = '5' THEN 'ZIP_EMI'
+           WHEN payment_instrument = '6' THEN 'UPI'
+           WHEN payment_instrument = '7' THEN 'CC'
+           WHEN payment_instrument = '8' THEN 'DC'
+           WHEN payment_instrument = '9' THEN 'CC_DC'
+           WHEN payment_instrument = '10' THEN 'UPI_COLLECT'
+           WHEN payment_instrument = '11' THEN 'EMANDATE'
+           WHEN payment_instrument = '12' THEN 'NET_BANKING'
+           ELSE NULL
+         end )                        AS Payment_Instrument,
+       mtmd.ext_ref_no                AS 'External_Refrence_Number'
+FROM   wallet_as_pg_ledger wapg
+       LEFT JOIN wallet_as_pg_ledger_metadata wapgm
+              ON ( wapg.id = wapgm.parentid )
+       LEFT JOIN txp t
+              ON ( t.orderid = wapg.orderid
+                   AND t.statecode BETWEEN 28 AND 68 )
+       LEFT JOIN merchant_txp_meta_data mtmd
+              ON ( t.id = mtmd.parent_id )
+       LEFT JOIN merchant
+              ON ( wapg.mid = merchant.mid )
+WHERE  ( wapg.mid IN (SELECT mid
+                      FROM   icici_payout_merchants
+                      WHERE  ispayoutenabled = 1)
+         AND IF(wapg.smid IS NOT NULL,
+             wapg.smid IN (SELECT mid
+                           FROM   icici_payout_merchants
+                           WHERE  ispayoutenabled = 1), (
+                 wapg.mid IN (SELECT mid
+                              FROM   icici_payout_merchants
+                              WHERE  ispayoutenabled = 1) )) )
+       AND wapg.updatedat >= Date(Now())
+       AND wapg.refundbatchid LIKE Concat('%', Date_format(Now(), '%Y%m%d'), '%'
+                                   )
+       AND ( ismarketplace != 'y'
+              OR ismarketplace IS NULL )
+       AND paymenttype NOT IN ( 2, 3, 4 )
+        AND (
+        (wapg.refundbatchid, wapg.mid) in (
+        select
+        batch_id, merchant_id
+        from
+        settlement_wapg
+        where
+        created_at >= curdate()
+        and status = 'calculated'
+        )
+        OR
+        (wapg.refundbatchid, wapg.smid) in (
+        select
+        batch_id, merchant_id
+        from
+        settlement_wapg
+        where
+        created_at >= curdate()
+        and status = 'calculated'
+        )
+        )
+
+ORDER  BY 1;"  | $MYSQL --login-path=mobinewcronmaster_RDS01 -D $DB | sed 's/\t/","/g;s/^/"/;s/$/"/;s/\n//g' >> /tmp/cronreports/working/WAPG_ICICI_Working_file_Non_Mplace_$CURRENTDATE.csv
 gzip  /tmp/cronreports/working/WAPG_ICICI_Working_file_Non_Mplace_$CURRENTDATE.csv
 cd /tmp/cronreports/
 TIMESTAMP=`date "+%Y-%m-%d %H-%M-%S"`
